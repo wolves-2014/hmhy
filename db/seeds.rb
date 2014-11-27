12bb0c54ecca4dc4e35a86549751da4f1d47850f
David Lamps & Richard Baptist <pair+luminous14.rpbaptist@devbootcamp.com>
2014-11-27 12:02:39 -0600

    created seeds, slight schema rework

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,53 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+feelings = ["numb","sad","depressed","lonely","anxious","stressed"]
+
+feelings.map! do | feeling |
+  Feeling.create!(word: feeling)
+end
+
+assessments = ["grief","addiction","anger","obsessive-compulsive","video game addiction","depression"]
+
+assessments.map!{|assessment| Assessment.create(word: assessment)}
+
+assessments[0].indications.create!(
+  [{feeling: feelings[0]},
+  {feeling: feelings[1]},
+  {feeling: feelings[3]},
+  {feeling: feelings[4]},
+  {feeling: feelings[5]}]
+  )
+
+assessments[1].indications.create!(
+  [{feeling: feelings[0]},
+  {feeling: feelings[3]},
+  {feeling: feelings[4]},
+  {feeling: feelings[5]}]
+  )
+
+assessments[2].indications.create!(
+  [{feeling: feelings[0]},
+  {feeling: feelings[4]},
+  {feeling: feelings[5]}]
+  )
+
+assessments[3].indications.create!(
+  [{feeling: feelings[4]},
+  {feeling: feelings[5]}]
+  )
+
+assessments[4].indications.create!(
+  [{feeling: feelings[1]},
+  {feeling: feelings[3]},
+  {feeling: feelings[4]}]
+  )
+
+assessments[5].indications.create!(
+  [{feeling: feelings[0]},
+  {feeling: feelings[1]},
+  {feeling: feelings[3]},
+  {feeling: feelings[4]}]
+  )
+
