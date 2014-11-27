2c9e80423fca2a60206980fd06f2e10497dfd61c
David Lamps & Richard Baptist <pair+luminous14.rpbaptist@devbootcamp.com>
2014-11-27 16:15:30 -0600

    created logic in controllers

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,7 +1,13 @@
 class Provider < ActiveRecord::Base
   has_many :competencies
+  has_many :assessments, through: :competencies
   has_many :residences
   has_many :locations, through: :residences
 
   validates :name, :profile_url, :phone_number, presence: true
+
+  def self.match(assessments)
+    providers = assessments.map{|assessment| assessment.providers.to_a}
+    providers.flatten.uniq
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-feelings = ["numb","sad","depressed","lonely","anxious","stressed"]
+feelings = ["numb", "sad", "depressed", "lonely", "anxious", "stressed"]
 
 feelings.map! do | feeling |
   Feeling.create!(word: feeling)
 end
 
-assessments = ["grief","addiction","anger","obsessive-compulsive","video game addiction","depression"]
+assessments = ["grief", "addiction", "anger", "obsessive-compulsive", "video game addiction", "depression"]
 
 assessments.map!{|assessment| Assessment.create(word: assessment)}
 
@@ -44,6 +44,7 @@ assessments[3].indications.create!(
 
 assessments[4].indications.create!(
   [{feeling: feelings[1]},
+  {feeling: feelings[2]},
   {feeling: feelings[3]},
   {feeling: feelings[4]}]
   )
@@ -52,6 +53,46 @@ assessments[5].indications.create!(
   [{feeling: feelings[0]},
   {feeling: feelings[1]},
   {feeling: feelings[3]},
-  {feeling: feelings[4]}]
+  {feeling: feelings[2]}]
   )
+
+providers = []
+
+20.times do
+  providers << Provider.create!(
+  title: Faker::Name.title,
+  name: Faker::Name.name,
+  photo_url: "http://lorempixel.com/150/200/cats/#{rand(1..10)}",
+  profile_url: Faker::Internet.url,
+  email: Faker::Internet.email,
+  phone_number: Faker::PhoneNumber.phone_number)
+end
+
+providers.each do |provider|
+  assessments = []
+  2-5.times do
+    assessments << Assessment.all.sample
+  end
+  assessments.uniq!
+  assessments.each do |assessment|
+    provider.competencies.create!(assessment: assessment)
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
