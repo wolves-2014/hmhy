71ced089d18e9a1bfbc3e1e532fdcf2f619d942b
Ryan Milstead & Jordan Schreuder <pair+ryanmilstead1.schreuderjo@devbootcamp.com>
2014-11-28 10:45:17 -0600

    initial search query returning specific results

---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,27 @@
+$(document).ready (function(){
+ $("#input-container a").click(function(e){
+  e.preventDefault();
+
+  var request = $.ajax({
+    url: "/providers",
+    type: 'GET',
+    data: {feeling: $(this).text()}
+  });
+
+  request.done(function(response){
+    console.log(response);
+    $("#results-container").replaceWith(response.html);
+  });
+
+  request.fail(function(response){
+    console.log(response);
+  });
+
+ });
+
+
+
+
+
+
+});
