39a27233bc01a3579cb32037242b8b50aecdfb6a
Richard Baptist & Ryan Milstead <pair+rpbaptist.ryanmilstead1@devbootcamp.com>
2014-12-01 20:09:21 -0600

    ip location working

--- app/models/location.rb
+++ app/models/location.rb
@@ -9,8 +9,6 @@ class Location < ActiveRecord::Base
 
   reverse_geocoded_by :latitude, :longitude do |obj,results|
     geo = results.first
-    # obj.update(zip_code: geo.postal_code, latitude: geo.latitude, longitude: geo.longitude)
-    binding.pry
     obj.zip_code = geo.postal_code
     obj.latitude = geo.latitude
     obj.longitude = geo.longitude
@@ -19,7 +17,8 @@ class Location < ActiveRecord::Base
 
   acts_as_copy_target
 
-  def by_ip_address
-
+  def Location.by_ip_address(location_data)
+    location_data = Geocoder.search('74.122.9.196').first if location_data.ip == "127.0.0.1"
+    Location.find_or_create_by(latitude: location_data.latitude, longitude: location_data.longitude)
   end
 end
