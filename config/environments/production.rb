1b30dc45db390869ec8f0dd35e4b36fd29a308a3
Jordan Schreuder <schreuderjo@gmail.com>
2014-12-03 16:26:02 -0600

    1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,17 +1,17 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "light-the-beacon.com" }
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: "smtp.mandrillapp.com",
-    port: 587,
-    domain: "light-the-beacon.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV['MANDRILL_USER_NAME'],
-    password: ENV['MANDRILL_PASSWORD']
-  }
+  # config.action_mailer.default_url_options = { host: "light-the-beacon.com" }
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address: "smtp.mandrillapp.com",
+  #   port: 587,
+  #   domain: "light-the-beacon.com",
+  #   authentication: "plain",
+  #   enable_starttls_auto: true,
+  #   user_name: ENV['MANDRILL_USER_NAME'],
+  #   password: ENV['MANDRILL_PASSWORD']
+  # }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
