95476e5a8aae88b3401e26a27ed6ecd8bbd537cf
Ryan Milstead <ryanmilstead1@gmail.com>
2014-12-02 22:16:53 -0600

    age and insurance added to seeds

---
+++ db/find_ages.rb
@@ -0,0 +1,15 @@
+require 'csv'
+require 'pry'
+require 'json'
+
+@filename = "temp_therapists.csv"
+all_ages = []
+CSV.readlines(@filename).each do |line|
+  unless line[8] == nil
+    age_array = JSON.parse line[8]
+    all_ages << age_array
+  end
+end
+binding.pry
+all_ages.flatten!.compact!.uniq!
+puts all_ages
--- db/seeds.rb
+++ db/seeds.rb
@@ -111,7 +111,19 @@ Location.copy_from 'db/us_postal_codes.csv'
 
 locations = Location.near(60606.to_s, 500).to_a
 
-@filename = "db/therapists_in_60000.csv"
+CSV.readlines("db/health_insurance_companies.csv").each do |line|
+  Insurance.create!(name: line[0])
+end
+
+Insurance.create!(name: "Out of Network")
+
+ages = ["Adolescents / Teenagers (14 to 19)", "Adults", "Elders (65+)", "Children (6 to 10)", "Preteens / Tweens (11 to 13)", "Toddlers / Preschoolers (0 to 6)"]
+
+ages.each do |age|
+  Age.create!(age_group: age)
+end
+
+@filename = "db/new_therapists.csv"
 counter = 0
 CSV.readlines(@filename, headers: true, header_converters: :symbol).each do |line|
   if location = Location.find_by(zip_code: line[6].to_i)
@@ -125,6 +137,7 @@ CSV.readlines(@filename, headers: true, header_converters: :symbol).each do |lin
       )
     new_provider.save(validate: false)
 
+    ## Create Competencies
     valid_competencies = []
     all_competencies = JSON.parse line[7]
     all_competencies.each do |competency|
@@ -138,7 +151,45 @@ CSV.readlines(@filename, headers: true, header_converters: :symbol).each do |lin
         new_provider.competencies.create!(assessment: assessment)
       end
     end
-    break if counter > 1000
+
+    ## Create Networks
+    valid_networks = []
+    if line[11] != nil
+      all_networks = JSON.parse line[11]
+      all_networks.each do |network|
+        if matched_network = network[/AARP|Aetna|Affinity|AFLAC|Alliance|ALTA|Altius|American Family|American Medical Security|American National|Amerigroup|AmeriHealth HMO|Ameritas|Arnett|Assurant|Asuris|Atlantis|Bankers Life and Casualty|Blue Cross and Blue Shield|Camrails a|Celtic|Centene|Cigna|Clear Choice|Cobalt|Companion|ConnectiCare|Conseco|Coventry|Cox|EmblemHealth|Fairmont|First Choice|FiServ|Fortis|GHI|Golden Rule|Great-West|Group Health Cooperative|Health Net|Health Plan Adminstrators|HealthAmerica|HealthMarkets|HealthPartners|HealthSpring|Highma|HIP|Humana|IHC|Kaiser Permanente|Kaleida|KPS|LifeWise|Medica|Medical Mutual of Ohio|MEGA|Mercy Health Plans of MO|Midwest Security|Molina|Mutual of Omaha|MVP|Neighborhood|ODS|OmniCare|Oxford|PacifiCare|PacificSource|Principal Financial Group|Providence|Rocky Mountain|Security Life|SelectHealth|Shelter|Sierra|Significa|Standard Security|State Farm|SummaCare|Thrivent|Time|Tricare|TUFTS|UNICARE|United Wisconsin|UnitedHealth Group|UnitedHealthcare|UnitedHealthOne|Unitrin|Unity|Univera|Universal American Corporation|VISTA|WellCare|Wellpath Select|WellPoint|WPS Health Insurance of Wisconsin|YKP/]
+          valid_networks << matched_network
+        end
+      end
+    end
+    unless valid_networks == []
+      valid_networks.uniq.each do |network|
+        insurance = Insurance.find_by(name: network)
+        new_provider.networks.create!(insurance: insurance)
+      end
+    else
+      new_provider.networks.create!(insurance: Insurance.find_by(name: "Out of Network") )
+    end
+
+    ## Create Targets
+    valid_targets = []
+    if line[8] !=nil
+      all_targets = JSON.parse line[8]
+      all_targets.each do |target|
+        if matched_target = target[/Adolescents \/ Teenagers \(14 to 19\)|Adults|Elders \(65\+\)|Children \(6 to 10\)|Preteens \/ Tweens \(11 to 13\)|Toddlers \/ Preschoolers \(0 to 6\)/]
+          valid_targets << matched_target
+        end
+      end
+    end
+    unless valid_targets == []
+      valid_targets.uniq.each do |target|
+        age = Age.find_by(age_group: target)
+        new_provider.targets.create!(age: age)
+        puts "#{counter}: provider: #{new_provider.name} age: #{age.age_group}"
+      end
+    end
+
+    break if counter == 1000
     counter += 1
     puts "Creating entry for Psychology Today user: #{line[0]}" if counter % 100 == 0
 
