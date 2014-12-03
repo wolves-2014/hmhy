2104cd304d68faed7f4e6757073cefc9d9c98cce
Jordan Schreuder <schreuderjo@gmail.com>
2014-12-03 11:28:19 -0600

    added perform delivers to config in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: "http://light-the-beacon.com" }
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
