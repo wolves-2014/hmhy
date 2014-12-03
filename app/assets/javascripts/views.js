2330b31a3c3dbb13f23951b2844e867786440958
Richard Baptist <rpbaptist@gmail.com>
2014-12-03 09:52:10 -0600

    fading out and in, set fixed height for input container

--- app/assets/javascripts/views.js
+++ app/assets/javascripts/views.js
@@ -11,16 +11,25 @@ function FeelingsView(container, header) {
   this.header = header;
 }
 
-FeelingsView.prototype.render = function(wordLevel, feelingsHTML) {
+FeelingsView.prototype.render = function(feelingsHTML) {
+  var wordLevel = $('.word-container').attr('id');
   if (parseInt(wordLevel) < 3) {
-    this.container.replaceWith(feelingsHTML);
-    this.updateHeader();
+    this.header.fadeOut('slow');
+    this.container.fadeOut('slow', function(){
+      this.container.html(feelingsHTML);
+      this.container.fadeIn('slow');
+      this.updateHeader();
+      this.header.fadeIn('slow');
+    }.bind(this));
   }
+}
+
+FeelingsView.prototype.fade = function() {
 }
 
 FeelingsView.prototype.updateHeader = function() {
   if (!this.header.hasClass('changed')) {
-    this.header.html("<h3 class='text-shadow'>You said you're feeling " + search.selectedFeeling + ".</h3><h2 class='text-shadow'>Do any of these apply to you?</h2>");
+    this.header.html("<h3 class='text-shadow no-top-margin'>You said you're feeling " + search.selectedFeeling + ".</h3><h2 class='text-shadow'>Do any of these apply to you?</h2>");
     this.header.addClass('changed');
   }
 }
